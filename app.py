b12de69b20305f77c6e32c18302eee271d2245b0
alethino <gneo0194@gmail.com>
2025-09-02 21:53:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 ### contact me
-**Contact for api :** [https://t.me/Neo_Galaxy](https://t.me/Neo_Galaxy)
-## telegram: @Neo_Galaxy
+**Contact for api :** [https://t.me/alethino](https://t.me/alethino)
+## telegram: @alethino
 # test api last version
